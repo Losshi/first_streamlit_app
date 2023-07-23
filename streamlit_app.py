4f1eb4d8382af304ac372514cb10414ce5cee842
Losshi <64606103+Losshi@users.noreply.github.com>
2023-07-24 00:25:54 +0530

    Get all rows

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,9 +4,9 @@ import snowflake.connector
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT * FROM PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST")
-my_data_row = my_cur.fetchone()
+my_data_rows = my_cur.fetchall()
 streamlit.text("The fruit load list contains:")
-streamlit.text(my_data_row)
+streamlit.text(my_data_rows)
 
 streamlit.title('My Mom\'s New Healthy Diner')
 
