156c2a2b24f2d46568fd84831b0f85f060cd60a9
Losshi <64606103+Losshi@users.noreply.github.com>
2023-07-24 00:26:37 +0530

    Get data from snowflake into a table format

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,8 +5,8 @@ my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT * FROM PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST")
 my_data_rows = my_cur.fetchall()
-streamlit.text("The fruit load list contains:")
-streamlit.text(my_data_rows)
+streamlit.header("The fruit load list contains:")
+streamlit.dataframe(my_data_rows)
 
 streamlit.title('My Mom\'s New Healthy Diner')
 
